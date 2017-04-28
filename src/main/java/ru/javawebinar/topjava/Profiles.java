1483041734ec24ba6b4aa80cf13955502215982a
lexfrei88 <alexey.frei@gmail.com>
2017-04-28 13:29:37 +0300

    patch 6 5

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -9,12 +9,24 @@ public class Profiles {
             POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
 
-    public static final String ACTIVE_DB = HSQL_DB;
-
     public static final String
             JDBC = "jdbc",
             JPA = "jpa",
             DATAJPA = "datajpa";
 
     public static final String REPOSITORY_IMPLEMENTATION = JPA;
+
+    public static String getActiveDbProfile() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return POSTGRES_DB;
+        } catch (ClassNotFoundException ex) {
+            try {
+                Class.forName("org.hsqldb.jdbcDriver");
+                return Profiles.HSQL_DB;
+            } catch (ClassNotFoundException e) {
+                throw new IllegalStateException("Could not resolve DB profile");
+            }
+        }
+    }
 }
