5f1cfb81f77c317147525d0f01fdb34055c4164a
lexfrei88 <alexey.frei@gmail.com>
2017-04-28 13:18:46 +0300

    patch 6 3

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -28,8 +28,8 @@ import static ru.javawebinar.topjava.UserTestData.*;
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(Profiles.HSQL_DB)
-public class UserServiceTest {
+@ActiveProfiles(Profiles.ACTIVE_DB)
+public class AbstractUserServiceTest {
 
     static {
         // Only for postgres driver logging
