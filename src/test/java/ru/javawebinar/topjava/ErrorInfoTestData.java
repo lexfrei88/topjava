74891296258757195631e4f743d9ebd0082c6f9e
lexfrei88 <alexey.frei@gmail.com>
2017-06-08 12:26:50 +0300

    HW 10 optional meal rest test for duplication datetime

--- src/test/java/ru/javawebinar/topjava/ErrorInfoTestData.java
+++ src/test/java/ru/javawebinar/topjava/ErrorInfoTestData.java
@@ -5,30 +5,37 @@ import ru.javawebinar.topjava.util.exception.ErrorInfo;
 
 public class ErrorInfoTestData {
     public static final ModelMatcher<ErrorInfo> ERROR_INFO_MODEL_MATCHER = ModelMatcher.of(ErrorInfo.class);
-    public static final String CAUSE_1 = "FieldException";
-    public static final String CAUSE_2 = "DataIntegrityViolationException";
+    public static final String CAUSE_FIELD_EXCEPTION = "FieldException";
+    public static final String CAUSE_DATA_INTEGRITY_VIOLATION_EXCEPTION = "DataIntegrityViolationException";
 
-    public static final String MEAL_URL = "http://localhost/rest/profile/meals/";
+    public static final ErrorInfo MEAL_CREATE_EXPECTED_ERROR = new ErrorInfo("http://localhost/rest/profile/meals/",
+            CAUSE_FIELD_EXCEPTION,
+            "description may not be empty<br>");
 
-    public static final String USER_URL = "http://localhost/rest/admin/users";
-    public static final String PROFILE_USER_URL = "http://localhost/rest/profile";
+    public static final ErrorInfo MEAL_UPDATE_EXPECTED_ERROR_WITH_ID = new ErrorInfo("http://localhost/rest/profile/meals/100002",
+            CAUSE_FIELD_EXCEPTION,
+            "description may not be empty<br>");
 
-    public static final String MEAL_DETAIL = "description may not be empty<br>";
+    public static final ErrorInfo USER_CREATE_EXPECTED_ERROR = new ErrorInfo("http://localhost/rest/admin/users/",
+            CAUSE_FIELD_EXCEPTION,
+            "password length must be between 5 and 2147483647<br>");
 
-    public static final String USER_DETAIL_1 = "password length must be between 5 and 2147483647<br>";
-    public static final String USER_DETAIL_2 = "name may not be empty<br>";
-    public static final String USER_DETAIL_3 = "User with this email already present in application";
+    public static final ErrorInfo USER_UPDATE_EXPECTED_ERROR_WITH_ID = new ErrorInfo("http://localhost/rest/admin/users/100000",
+            CAUSE_FIELD_EXCEPTION,
+            "name may not be empty<br>");
 
-    public static final ErrorInfo MEAL_CREATE_EXPECTED_ERROR = new ErrorInfo(MEAL_URL, CAUSE_1, MEAL_DETAIL);
+    public static final ErrorInfo PROFILE_USER_UPDATE_EXPECTED_ERROR_WITH_ID = new ErrorInfo(
+            "http://localhost/rest/profile",
+            CAUSE_FIELD_EXCEPTION,
+            "name may not be empty<br>");
 
-    public static final ErrorInfo MEAL_UPDATE_EXPECTED_ERROR_WITH_ID = new ErrorInfo(MEAL_URL + 100002, CAUSE_1, MEAL_DETAIL);
+    public static final ErrorInfo USER_WITH_EQUAL_EMAIL_EXPECTED_ERROR = new ErrorInfo(
+            "http://localhost/rest/admin/users",
+            CAUSE_DATA_INTEGRITY_VIOLATION_EXCEPTION,
+            "User with this email already present in application");
 
-    public static final ErrorInfo USER_CREATE_EXPECTED_ERROR = new ErrorInfo(USER_URL + '/', CAUSE_1, USER_DETAIL_1);
-
-    public static final ErrorInfo USER_UPDATE_EXPECTED_ERROR_WITH_ID = new ErrorInfo(USER_URL + '/' + 100000, CAUSE_1, USER_DETAIL_2);
-
-    public static final ErrorInfo PROFILE_USER_UPDATE_EXPECTED_ERROR_WITH_ID = new ErrorInfo(PROFILE_USER_URL, CAUSE_1, USER_DETAIL_2);
-
-    public static final ErrorInfo USER_WITH_EQUAL_EMAIL_EXPECTED_ERROR = new ErrorInfo(USER_URL, CAUSE_2, USER_DETAIL_3);
-
+    public static final ErrorInfo MEAL_WITH_EQUAL_DATETIME_EXPECTED_ERROR = new ErrorInfo(
+            "http://localhost/rest/profile/meals",
+            CAUSE_DATA_INTEGRITY_VIOLATION_EXCEPTION,
+            "Meal for this date-time has already exist in application");
 }
--- src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTestNotTransactional.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTestNotTransactional.java
@@ -1,21 +1,36 @@
 package ru.javawebinar.topjava.web.meal;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import ru.javawebinar.topjava.service.MealService;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.ErrorInfo;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.json.JsonUtil;
 
-import static ru.javawebinar.topjava.MealTestData.getCreated;
+import java.time.Month;
+
+import static java.time.LocalDateTime.of;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static ru.javawebinar.topjava.ErrorInfoTestData.ERROR_INFO_MODEL_MATCHER;
+import static ru.javawebinar.topjava.ErrorInfoTestData.MEAL_WITH_EQUAL_DATETIME_EXPECTED_ERROR;
+import static ru.javawebinar.topjava.TestUtil.userHttpBasic;
+import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.web.meal.MealRestController.REST_URL;
 
 public class MealRestControllerTestNotTransactional extends AbstractControllerTest {
 
-    @Autowired
-    private MealService service;
-
-    @Test(expected = DataIntegrityViolationException.class)
+    @Test
     public void testCreateWithEqualDateTime() throws Exception {
-        service.save(getCreated(), 100000);
-        service.save(getCreated(), 100000);
+        Meal create = new Meal(of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500);
+
+        ResultActions action = mockMvc.perform(post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(create))
+                .with(userHttpBasic(USER)));
+
+        ErrorInfo returned = ERROR_INFO_MODEL_MATCHER.fromJsonAction(action);
+
+        ERROR_INFO_MODEL_MATCHER.assertEquals(MEAL_WITH_EQUAL_DATETIME_EXPECTED_ERROR, returned);
     }
 }
